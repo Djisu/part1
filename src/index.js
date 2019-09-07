ec4d62ea8eb35eaf3613c432527be61a0868bd2c
Djisu <pfleischer2002@yahoo.co.uk>
2019-09-07 02:20:05 -0700

    Variable passed directly: not as props

--- src/index.js
+++ src/index.js
@@ -10,38 +10,6 @@ const App = () => {
     // const part3 = 'State of a component'
     // const exercises3 = 14
     
-
-
-    // const Header = (props) => {
-    //     return (
-    //         <div>
-    //             <p>{props.course}</p>
-    //         </div>   
-    //     )      
-    // }
-   
-    // const Part = (props) => {
-    //     return (
-    //         <div>
-    //             <p>{props.part1} {props.exercises1}</p>
-    //         </div>
-    //     )
-    // }
-    // const Content = (props) =>{
-    //     return (
-    //         <div>
-    //             <Part part1={part1} exercises1={exercises1} />
-    //             <Part part1={part1} exercises1={exercises1} />
-    //             <Part part1={part1} exercises1={exercises1} />
-    //         </div>
-    //     )
-    // }
-    // const Total = (props) => {
-    //     return (
-    //         <p>{props.total}</p>
-    //     )
-    // }
- 
     const course = 'Half Stack application development'
     const parts = [
       {
@@ -57,19 +25,42 @@ const App = () => {
         exercises: 14
       }
     ]
+
+    const Header = ({course}) => {
+        return (
+            <div>
+                <p>{course}</p>
+            </div>   
+        )      
+    }
+   
+   
+    const Content = ({parts}) =>{
+        return (
+            <div>
+                <p>{parts[0].name} {parts[0].exercises}</p>
+                <p>{parts[1].name} {parts[1].exercises}</p>
+                <p>{parts[2].name} {parts[2].exercises}</p>
+            </div>
+        )
+    }
+    const Total = (props) => {
+        return (
+            <p>{parts[0].exercises +parts[1].exercises + parts[2].exercises}</p>
+        )
+    }
+ 
+    
    // const total = part1.exercises1 + part2.exercises2 + part3.exercises3
 
     return (
         <div>
-            {/* <Header course={course} />
-            <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
-            <Total total={total} /> */}
-            <p>{course}</p>
-            
-            <p>{parts[0].name} {parts[0].exercises}</p>
-            <p>{parts[1].name} {parts[1].exercises}</p>
-            <p>{parts[2].name} {parts[2].exercises}</p>
-            <p>{parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+               <div>
+                    <Header course={course} />
+                    <Content parts={parts}  />
+                    <Total parts={parts} />
+               </div>
+           
         </div>
 
     )
